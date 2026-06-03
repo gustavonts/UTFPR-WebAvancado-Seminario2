b690af07d246f7d470feb55c46584ff9a033f150
Gustavo Guedes <gugaguedes11@gmail.com>
2026-06-02 21:36:53 -0300

    Incluida a primeira entidade Usuário

---
+++ src/Entity/Usuario.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UsuarioRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UsuarioRepository::class)]
+class Usuario
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nome = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $senha = null;
+
+    #[ORM\Column]
+    private ?bool $ativo = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNome(): ?string
+    {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): static
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getAtivo(): ?string
+    {
+        return $this->ativo;
+    }
+
+    public function setAtivo(string $ativo): static
+    {
+        $this->ativo = $ativo;
+
+        return $this;
+    }
+
+    public function getSenha(): ?string
+    {
+        return $this->senha;
+    }
+
+    public function setSenha(string $senha): static
+    {
+        $this->senha = $senha;
+
+        return $this;
+    }
+}
