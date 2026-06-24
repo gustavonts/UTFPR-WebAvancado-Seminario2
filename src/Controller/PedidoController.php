184da29b5f18614f882712b3ba7223024b91b168
Gustavo Guedes <gugaguedes11@gmail.com>
2026-06-24 14:57:02 -0300

    feat(pedido): implementa funcionalidade de alteração de status do pedido

--- src/Controller/PedidoController.php
+++ src/Controller/PedidoController.php
@@ -1,7 +1,10 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\Order;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\OrderStatusHistory;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,19 +14,72 @@ use App\Repository\OrderRepository;
 final class PedidoController extends AbstractController
 {
     #[Route('/pedidos', name:'app_pedidos')]
-        public function index(
-            OrderRepository $repository
-        ): Response
-        {
+    public function index(
+        OrderRepository $repository
+    ): Response
+    {
 
-            $pedidos = $repository->findAll();
+        $pedidos = $repository->findAll();
 
 
-            return $this->render(
-                'pedido/index.html.twig',
-                [
-                    'pedidos'=>$pedidos
-                ]
-            );
-        }
+        return $this->render(
+            'pedido/index.html.twig',
+            [
+                'pedidos'=>$pedidos
+            ]
+        );
+    }
+    
+    #[Route('/pedidos/{id}/status', name:'app_pedido_status')]
+    public function status(
+        Order $pedido,
+        Request $request,
+        EntityManagerInterface $entityManager
+    ): Response
+    {
+
+        $novoStatus = $request->request->get('status');
+
+
+        $historico = new OrderStatusHistory();
+
+        $historico->setOrder($pedido);
+
+        $historico->setOldStatus(
+            $pedido->getStatus()
+        );
+
+
+        $historico->setNewStatus(
+            $novoStatus
+        );
+
+
+        $historico->setChangedAt(
+            new \DateTimeImmutable()
+        );
+
+
+        $historico->setChangedBy(
+            $this->getUser()
+        );
+
+
+        $pedido->setStatus(
+            $novoStatus
+        );
+
+
+        $entityManager->persist($historico);
+
+        $entityManager->flush();
+
+
+        return $this->redirectToRoute(
+            'app_pedido_show',
+            [
+                'id'=>$pedido->getId()
+            ]
+        );
+    }
 }
