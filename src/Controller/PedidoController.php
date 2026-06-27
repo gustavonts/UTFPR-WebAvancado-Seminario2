6982ca13e9b0a14b50800553ee0c6904c82988dc
Gustavo Guedes <gugaguedes11@gmail.com>
2026-06-27 17:29:29 -0300

    feat(pedido): adiciona funcionalidade de criação e gerenciamento de pedidos, incluindo upload de documentos e histórico de status

--- src/Controller/PedidoController.php
+++ src/Controller/PedidoController.php
@@ -9,6 +9,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\OrderRepository;
+use App\Form\PedidoType;
+use App\Entity\OrderDocument;
 
 
 final class PedidoController extends AbstractController
@@ -79,6 +81,156 @@ final class PedidoController extends AbstractController
             'app_pedido_show',
             [
                 'id'=>$pedido->getId()
+            ]
+        );
+    }
+
+    #[Route('/pedidos/novo', name: 'app_pedido_new')]
+    public function new(
+        Request $request,
+        EntityManagerInterface $entityManager
+    ): Response
+    {
+
+        $pedido = new Order();
+
+
+        $form = $this->createForm(
+            PedidoType::class,
+            $pedido
+        );
+
+
+        $form->handleRequest($request);
+
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+
+            $pedido->setCreatedAt(
+                new \DateTimeImmutable()
+            );
+
+
+            $entityManager->persist($pedido);
+
+            $entityManager->flush();
+
+
+            return $this->redirectToRoute(
+                'app_pedido_show',
+                [
+                    'id' => $pedido->getId()
+                ]
+            );
+        }
+
+
+        return $this->render(
+            'pedido/new.html.twig',
+            [
+                'form' => $form->createView()
+            ]
+        );
+    }
+
+    #[Route('/pedidos/excluir/{id}', name: 'app_pedido_delete')]
+    public function delete(
+        Order $pedido,
+        EntityManagerInterface $entityManager
+    ): Response
+    {
+        $entityManager->remove($pedido);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_pedidos');
+    }
+
+    #[Route('/pedidos/{id}/documento', name: 'app_pedido_documento', methods: ['POST'])]
+    public function uploadDocumento(
+        Order $pedido,
+        Request $request,
+        EntityManagerInterface $entityManager
+    ): Response
+    {
+
+        $arquivo = $request->files->get('documento');
+
+
+
+        if (!$arquivo) {
+            return $this->redirectToRoute(
+                'app_pedido_show',
+                ['id'=>$pedido->getId()]
+            );
+        }
+
+        if (!$arquivo->isValid()) {
+
+            dd($arquivo->getErrorMessage());
+
+        }
+
+        if ($arquivo) {
+
+
+           $nomeArquivo = uniqid()
+                .'.'
+                .$arquivo->getClientOriginalExtension();
+
+
+            $mimetype = $arquivo->getMimeType();
+
+            $nomeOriginal = $arquivo->getClientOriginalName();
+
+
+            $destino = $this->getParameter('kernel.project_dir')
+                . '/public/uploads';
+
+
+            if (!is_dir($destino)) {
+                mkdir($destino, 0777, true);
+            }
+
+
+            $arquivo->move(
+                $destino,
+                $nomeArquivo
+            );
+
+
+            $documento = new OrderDocument();
+
+            $documento->setOriginalName(
+                $nomeOriginal
+            );
+
+            $documento->setFilePath(
+                $nomeArquivo
+            );
+
+
+            $documento->setMimeType(
+                $mimetype
+            );
+
+
+            $documento->setOrder(
+                $pedido
+            );
+
+
+            $entityManager->persist($documento);
+
+            $entityManager->flush();
+
+        }
+
+
+        return $this->redirectToRoute(
+            'app_pedido_show',
+            [
+                'id' => $pedido->getId()
             ]
         );
     }
---
+++ src/Form/PedidoType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Order;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PedidoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('number')
+            ->add('customerName')
+            ->add('customerEmail')
+            ->add('totalAmount')
+            ->add('status', ChoiceType::class, [
+                'choices' => [
+
+                    'Aguardando pagamento' => 'AGUARDANDO_PAGAMENTO',
+
+                    'Pago' => 'PAGO',
+
+                    'Em transporte' => 'EM_TRANSPORTE',
+
+                    'Entregue' => 'ENTREGUE',
+
+                    'Cancelado' => 'CANCELADO',
+
+                ],
+            ]);
+    }
+
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Order::class,
+        ]);
+    }
+}
